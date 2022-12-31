9597bda82d62869f9f22f3ca3ccde9c03c5f4cb9
ll-nick <github@le-large.net>
2022-12-31 11:46:43 +0100

    Automated build.

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build \
-  --pull \
-  -t nickll/librespot \
-  .
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm --name librespot -p 5353:5353 -v /tmp/librespot:/data -e SPOTIFY_NAME=Snapcast -e SPOTIFY_USER=<user> -e SPOTIFY_PASSWORD=<pw> nickll/librespot
